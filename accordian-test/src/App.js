b3e81f8328b087644c21dc25a006e5f19f110796
Janaka Abeytunge <janakaabeytunge@Janakas-MacBook-Pro.local>
2022-12-20 17:53:14 +0100

    first Commit

---
+++ accordian-test/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AccordianCmp from './AccordianCmp';
+
+function App() {
+  return (
+    <div className="App">
+     <AccordianCmp></AccordianCmp>
+    </div>
+  );
+}
+
+export default App;
